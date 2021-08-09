2018853aa936881052b65a548059c626dca0ef56
DESKTOP-QFHSCL5\Robert <robin_yo@yahoo.com>
2021-08-09 17:17:33 +0300

    Add header, change app css in scss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
+import './App.scss';
+import { Switch, Route } from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import { Switch, Route } from 'react-router-dom';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/shop' component={ShopPage} />
         <Route exact path='/' component={HomePage} />
